bb166d09ec6d6e44e51dd1a9b070e417c6c8595d
Snatiolam <salzatec1@alzate-pc.localdomain>
2020-09-28 13:27:15 -0500

    Fixing some errors during installation

--- install.sh
+++ install.sh
@@ -1,16 +1,50 @@
 #!/bin/bash
 
 
-BIN_DIR="./.local/bin"
-BIN_INSTALL="$HOME/.local/bin"
+ROOT_DIR="."
+INSTALL_DIR="$HOME"
 
 install_binaries(){
-    [ ! -d "$BIN_INSTALL" ] && mkdir -p $BIN_INSTALL
-    for file in "$BIN_DIR/*"
+    echo "Installing shell scripts in ~/.local/bin"
+    INSTALL_BIN="$INSTALL_DIR/.local/bin"
+    [ ! -d "$INSTALL_BIN" ] && mkdir -p "$INSTALL_BIN"
+    for file in "$ROOT_DIR/.local/bin/*"
     do
-        cp $file $BIN_INSTALL
+        sudo cp $file $INSTALL_BIN
     done
+    echo "Shell scripts installed"
 }
 
-install_binaries
+install_vimrc(){
+    echo "Installing vimrc file"
+    cp .vimrc $HOME
+    echo "Installed vimrc"
+}
+
+install_i3_config(){
+    [ ! -d "$INSTALL_DIR/.config/i3" ] && sudo mkdir -p "$INSTALL_DIR/.config/i3"
+    cp "$ROOT_DIR/.config/i3/config" "$INSTALL_DIR/.config/i3"
+}
+
+install_i3blocks_config(){
+    [ ! -d "$INSTALL_DIR/.config/i3blocks" ] && mkdir -p "$INSTALL_DIR/.config/i3blocks"
+    cp "$ROOT_DIR/.config/i3blocks/config" "$INSTALL_DIR/.config/i3blocks/"
+    INSTALL_SCRIPTS="$ROOT_DIR/.config/i3blocks/scripts"
+    [ ! -d "$INSTALL_DIR/.config/i3blocks/scripts" ] && sudo mkdir -p "$INSTALL_DIR/.config/i3blocks/scripts"
+    for file in "$INSTALL_SCRIPTS/*"
+    do
+        sudo cp $file "$INSTALL_DIR/.config/i3blocks/scripts"
+    done
+    
+}
+
+main(){
+    # install_binaries
+    # install_vimrc
+    # install_i3_config
+    install_i3blocks_config
+}
+
+main
+
 exit 0
