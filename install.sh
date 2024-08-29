b13d47a68da2c8d22fdaed10c7dc14d7defebd11
Santiago Alzate <santiago.alzate3@gmail.com>
2024-08-29 10:57:04 -0500

    Added install for vimrc

--- install.sh
+++ install.sh
@@ -16,13 +16,26 @@ install_binaries(){
 }
 
 install_vimrc(){
-    echo "Installing vimrc file"
-    cp .vimrc "$HOME/"
-    echo "Installed vimrc"
+    echo "Installing VIM configuration files"
+    FILE="$HOME/.vimrc"
+    if [ -f "$FILE" ]; then
+        echo "File $FILE exist"
+        read -p "Do you want to overwrite it? Y/N -> " -n 1 -r
+        if [[ "$REPLY" =~ ^[Yy]$ ]]
+        then
+            printf "\nFile is going to be replaced"
+            cp .vimrc "$FILE"
+        fi
+        printf "\n"
+    else
+        echo "File $FILE does not exist."
+        cp .vimrc "$FILE"
+    fi
 }
 
 install_zsh_files(){
     ZSHRC="$HOME/.zshrc"
+    echo "Installing ZSH configuration files"
     if [ -f "$ZSHRC" ]; then
         echo "File $ZSHRC exist"
         read -p "Do you want to overwrite it? Y/N -> " -n 1 -r
@@ -35,14 +48,14 @@ install_zsh_files(){
         fi
         printf "\n"
     else
-        echo "File does not exist"
+        echo "File $ZSHRC does not exist"
         ln -s $(realpath "$ROOT_DIR/.zshrc") "$ZSHRC"
         # cp "$ROOT_DIR/.zprofile" "$HOME/"
     fi
 }
 main(){
     # install_binaries
-    # install_vimrc
+    install_vimrc
     install_zsh_files
 }
 
